236079fd7d6f8b9e1662d05c83ea93fa94c0f4b4
David Sanders <dsanderscanada@gmail.com>
2017-05-25 14:38:21 -0400

    Temporarily set requests to use http

--- InitializationPackage/__init__.py
+++ InitializationPackage/__init__.py
@@ -13,4 +13,4 @@ app.config["AGENT_PORT"] = 5000
 app.config["AGENT_DEBUG"] = True
 app.config["LOGGING_FORMAT"] = "%(asctime)s %(levelname)s: %(message)s"
 app.config["LOGGING_LEVEL"] = 10
-app.config["COWBULL_URL"] = "https://cowbull-test-project.appspot.com/v0_1/{}"
+app.config["COWBULL_URL"] = "http://cowbull-test-project.appspot.com/v0_1/{}"
