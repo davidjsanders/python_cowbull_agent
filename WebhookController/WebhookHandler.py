dbbd9d8f9b3beb6939ac8cfb289431c97c299f16
David Sanders <dsanderscanada@gmail.com>
2017-05-25 09:07:57 -0400

    Refining structure

--- WebhookController/WebhookHandler.py
+++ WebhookController/WebhookHandler.py
@@ -9,60 +9,59 @@ from werkzeug.exceptions import BadRequest
 
 
 class WebhookHandler(MethodView):
+    webhook_response = {
+        "speech": None,
+        "displayText": None,
+        "source": "cowbull-agent",
+        "result": None,
+        "parameters": None
+    }
+
     def post(self):
         cowbull_url = app.config.get("COWBULL_URL", None)
 
         logging.debug("Processing webhook")
         logging.debug("Game server is {}".format(cowbull_url))
 
-        webhook_response = {
-            "speech": None,
-            "displayText": None,
-            "source": "cowbull-agent",
-            "result": None,
-            "parameters": None
-        }
-
         request_mimetype = request.headers.get('Content-Type', None)
         logging.debug("Content type of request is: {}".format(request_mimetype))
 
         if request_mimetype is None\
         or request_mimetype.lower() != "application/json":
-            webhook_response = {
+            self.webhook_response = {
                 "status": 400,
                 "message": "content-type must be explicitly specified "
                            "as application/json"
             }
-            return self._build_error_response(response=webhook_response)
+            return self._build_error_response(response=self.webhook_response)
 
         json_string = request.get_json(silent=True, force=True)
 
         if json_string is None:
-            webhook_response = {
+            self.webhook_response = {
                 "status": 400,
                 "message": "No JSON data was provided"
             }
-            return self._build_error_response(response=webhook_response)
+            return self._build_error_response(response=self.webhook_response)
 
         webhook_result = json_string.get('result', None)
         if webhook_result is not None:
-            webhook_response["parameters"] = webhook_result.get('parameters', None)
-            webhook_response["action"] = webhook_result.get('action', None)
+            self.webhook_response["parameters"] = webhook_result.get('parameters', None)
+            self.webhook_response["action"] = webhook_result.get('action', None)
         else:
-            webhook_response = {
+            self.webhook_response = {
                 "status": 400,
                 "message": "No result data. The request from api.ai was badly formed!"
             }
-            return self._build_error_response(response=webhook_response)
+            return self._build_error_response(response=self.webhook_response)
 
-
-        webhook_response["speech"] = webhook_response["displayText"] = "Hello!"
+        self.webhook_response["speech"] = self.webhook_response["displayText"] = "Hello!"
         #webhook_response["payload"] = json_string
 
         return Response(
             status=200,
             mimetype="application/json",
-            response=json.dumps(webhook_response)
+            response=json.dumps(self.webhook_response)
         )
 
     def _build_error_response(
