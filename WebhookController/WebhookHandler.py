f024ea366f48feaf8c80029e024a7b3b1aa5c53e
David Sanders <dsanderscanada@gmail.com>
2017-05-25 15:55:34 -0400

    Implement make guess

--- WebhookController/WebhookHandler.py
+++ WebhookController/WebhookHandler.py
@@ -104,7 +104,8 @@ class WebhookHandler(MethodView):
         key = [n["parameters"]["key"] for n in _contexts if n["name"] == "key"][0]
         digits_required = [n["parameters"]["digits"] for n in _contexts if n["name"] == "digits"][0]
         guesses = [n["parameters"]["guesses"] for n in _contexts if n["name"] == "guesses"][0]
-        digits_guessed = _parameters.get("digitlist", None)
+        digits_guessed = [int(n) for n in _parameters.get("digitlist", None)]
+#        digits_guessed = _parameters.get("digitlist", None)
 
         logging.debug('Key: {}. Digits required: {}. Guesses: {}'.format(key, digits_required, guesses))
         logging.debug('Digits guessed are: {}'.format(digits_guessed))
