a8eee6fa62dd30b63c9d139dfa42ffcf8f4ac0e2
David Sanders <dsanderscanada@gmail.com>
2017-05-25 15:39:23 -0400

    Implement make guess

--- WebhookController/WebhookHandler.py
+++ WebhookController/WebhookHandler.py
@@ -101,9 +101,9 @@ class WebhookHandler(MethodView):
         if _contexts == []:
             raise ValueError("For some reason, no contexts are specified!")
 
-        key = [n["name"] for n in a if n["name"] == "key"][0]
-        digits_required = [n["name"] for n in a if n["name"] == "digits"][0]
-        guesses = [n["name"] for n in a if n["name"] == "guesses"][0]
+        key = [n["name"] for n in _contexts if n["name"] == "key"][0]
+        digits_required = [n["name"] for _contexts in a if n["name"] == "digits"][0]
+        guesses = [n["name"] for n in _contexts if n["name"] == "guesses"][0]
 
         logging.debug('Key: {}. Digits required: {}. Guesses: {}'.format(key, digits_required, guesses))
 
