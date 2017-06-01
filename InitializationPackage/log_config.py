9b130de9cd0e8237e1f9c95f960576d10995064a
David Sanders <dsanderscanada@gmail.com>
2017-06-01 11:04:58 -0400

    Update configuration management

--- InitializationPackage/log_config.py
+++ InitializationPackage/log_config.py
@@ -7,8 +7,8 @@ def log_config(app=None):
         raise ValueError("APP is undefined!!")
 
     logging.debug("Agent Host is {}".format(app.config["AGENT_HOST"]))
-    logging.debug("Agent Port is {}".format(app.config["AGENT_PORT"]))
-    logging.debug("Agent Debug is {}".format(app.config["AGENT_DEBUG"]))
+    logging.debug("Agent Port is {} (** value ignored by Docker **)".format(app.config["AGENT_PORT"]))
+    logging.debug("Agent Debug is {} (** value ignored by Docker **)".format(app.config["AGENT_DEBUG"]))
     logging.debug("Logging format is {}".format(app.config["LOGGING_FORMAT"]))
     logging.debug("Logging level is {}".format(app.config["LOGGING_LEVEL"]))
     logging.debug("Cowbull URL is {}".format(app.config["COWBULL_URL"]))
